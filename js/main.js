90f92f46177732bd575b4bf685ecf03592463ccb
iamkevlavoie <98438714+iamkevlavoie@users.noreply.github.com>
2022-07-11 09:36:13 -0600

    navbar disapear when you scroll up or down

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@ const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 const hireButton = document.querySelector("#hiremebutton");
 
+var prevScrollpos = window.pageYOffset;
+
+
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
@@ -14,3 +17,17 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click",(
     hireButton.classList.remove("active");
 
 }))
+
+window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        hamburger.classList.remove("active");
+        navMenu.classList.remove("active");
+        hireButton.classList.remove("active");
+    } else {
+        hamburger.classList.remove("active");
+        navMenu.classList.remove("active");
+        hireButton.classList.remove("active");
+    }
+    prevScrollpos = currentScrollPos;
+  }
